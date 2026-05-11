a94b0f00b711cc7f0e0d53a9ec1ed332104441b2
Chagon-Nehe <cgondwe@byupathway.edu>
2026-05-11 11:53:32 +0200

    added a function to add items in the cart

--- src/js/product.js
+++ src/js/product.js
@@ -1,10 +1,18 @@
-import { setLocalStorage } from "./utils.mjs";
+import { setLocalStorage, getLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
+// add product to cart function
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  let cartItems = getLocalStorage("so-cart"); // get cart items from local storage
+  if (!Array.isArray(cartItems)) {    // if cart items is not an array, initialize it as an empty array
+    cartItems = [];
+  }
+
+  cartItems.push(product); // add product to cart items array
+
+  setLocalStorage("so-cart", cartItems);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
