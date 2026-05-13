351707fd7a74416ae53ad87f16f7f90bc565e43b
anichokwenda <anichokwenda@gmail.com>
2026-05-13 15:11:27 +0200

    second week

--- src/js/ProductData.mjs
+++ src/js/ProductData.mjs
@@ -11,13 +11,17 @@ export default class ProductData {
     this.category = category;
     this.path = `../json/${this.category}.json`;
   }
-  getData() {
-    return fetch(this.path)
-      .then(convertToJson)
-      .then((data) => data);
+
+  async getData() {
+    const response = await fetch(this.path);
+    return convertToJson(response);
   }
+
   async findProductById(id) {
     const products = await this.getData();
-    return products.find((item) => item.Id === id);
+
+    return products.find(
+      (item) => item.Id.toLowerCase() === id.toLowerCase()
+    );
   }
 }
---
+++ src/js/ProductDetails.mjs
@@ -0,0 +1,70 @@
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
+
+export default class ProductDetails {
+  constructor(productId, dataSource) {
+    this.productId = productId;
+    this.product = {};
+    this.dataSource = dataSource;
+  }
+
+  async init() {
+    this.product = await this.dataSource.findProductById(this.productId);
+    this.renderProductDetails();
+
+    const addToCartBtn = document.getElementById("addToCart");
+    if (addToCartBtn) {
+      addToCartBtn.addEventListener("click", this.addProductToCart.bind(this));
+    }
+  }
+
+  addProductToCart() {
+    if (!this.product) return;
+    const cartItems = getLocalStorage("so-cart") || [];
+    cartItems.push(this.product);
+    setLocalStorage("so-cart", cartItems);
+    alert(`${this.product.NameWithoutBrand || this.product.Name} added to cart!`);
+  }
+
+  renderProductDetails() {
+  const product = this.product;
+
+  const container = document.querySelector(".product-detail");
+
+  if (!product || !product.Id) {
+    container.innerHTML = "<p>Product not found</p>";
+    return;
+  }
+
+  container.innerHTML = `
+    <h3>${product.Brand.Name}</h3>
+
+    <h2 class="divider">
+      ${product.NameWithoutBrand}
+    </h2>
+
+    <img
+      class="divider"
+      src="${product.Image}"
+      alt="${product.NameWithoutBrand}"
+    />
+
+    <p class="product-card__price">
+      $${product.FinalPrice}
+    </p>
+
+    <p class="product__color">
+      ${product.Colors[0].ColorName}
+    </p>
+
+    <p class="product__description">
+      ${product.DescriptionHtmlSimple}
+    </p>
+
+    <div class="product-detail__add">
+      <button id="addToCart">
+        Add to Cart
+      </button>
+    </div>
+  `;
+  }
+}
