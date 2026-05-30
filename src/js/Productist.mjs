a679696b7ae23998adda4a2717918865bc3c2b40
educalle23 <edu.lds.23@gmail.com>
2026-05-30 16:53:17 -0500

    Product List sort added

--- src/js/Productist.mjs
+++ src/js/Productist.mjs
@@ -18,11 +18,47 @@ export default class ProductList {
     this.category = category;
     this.dataSource = dataSource;
     this.listElement = listElement;
+    this.products = [];
   }
 
   async init() {
-    const list = await this.dataSource.getData();
-    this.renderList(list);
+    // const list = await this.dataSource.getData();
+    // this.renderList(list);
+    this.products = await this.dataSource.getData(this.category);
+    console.log(this.products);
+
+    this.renderList(this.products);
+
+    const sortSelect = document.getElementById("sortProducts");
+
+    sortSelect.addEventListener("change", (e) => {
+      // console.log(e.target.value);
+      this.sortProducts(e.target.value);
+    });
+  }
+
+  sortProducts(sortType) {
+    const sortedProducts = [...this.products];
+
+    switch (sortType) {
+      case "name-asc":
+        sortedProducts.sort((a, b) => a.Name.localeCompare(b.Name));
+        break;
+
+      case "name-desc":
+        sortedProducts.sort((a, b) => b.Name.localeCompare(a.Name));
+        break;
+
+      case "price-asc":
+        sortedProducts.sort((a, b) => a.FinalPrice - b.FinalPrice);
+        break;
+
+      case "price-desc":
+        sortedProducts.sort((a, b) => b.FinalPrice - a.FinalPrice);
+        break;
+    }
+
+    this.renderList(sortedProducts);
   }
 
   renderList(list) {
@@ -30,9 +66,12 @@ export default class ProductList {
     // this.listElement.insertAdjacentHTML("afterbegin", htmlStrings.join(""));
 
     // apply use new utility function instead of the commented code above
-    renderListWithTemplate(productCardTemplate, this.listElement, list, "afterbegin",
-      true);
-
+    renderListWithTemplate(
+      productCardTemplate,
+      this.listElement,
+      list,
+      "afterbegin",
+      true,
+    );
   }
-
 }
